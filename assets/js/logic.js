d37c5d7e055ef0250ed41face3f3e00c173d0a81
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-01-20 16:52:19 +0000

    Added function ideas to Logic JS file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,7 +12,35 @@ let startButton = document.getElementById("start");
 let initialElement = document.getElementById("initials");
 let feedBackElement = document.getElementById("feedback");
 
+//Added audio for correct & incorrect answer submissions:
 let sfxRight = new Audio("assets/sfx/correct.wav");
 let sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
+//Quiz fuctions:
+function getQuestion(){
 
+}
+
+function questionClick(){
+
+}
+
+function startQuiz(){
+
+}
+
+function quizEnd(){
+
+}
+
+function clockTick(){
+
+}
+
+function saveHighScore(){
+
+}
+
+function checkForEnter(event){
+
+}
