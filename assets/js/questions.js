a325208a65f31e10b33ddc89a40ade18454be6c1
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-01-20 16:35:29 +0000

    Added 3 questions to my questions JS file

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        "title": "Common Data Types DO NOAT include",
+        "choices": ["String", "Booleans", "Alerts", "Numbers"],
+        "answer": "Alerts"
+    },
+    {
+        "title": "Conditionals keywords are:",
+        "choices": ["if", "for", "while", "for each"],
+        "answer": "if"
+    },
+    {
+        "title": "What is the Capital of South Africa",
+        "choices": ["London", "Pretoria", "Paris", "Durban"],
+        "answer": "Pretoria"
+    }
+]
