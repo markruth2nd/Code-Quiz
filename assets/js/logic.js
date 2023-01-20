4dccb4ce12e6135093a78ef0f3d1797f1997f9b9
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-01-20 16:45:17 +0000

    Added variables linking to HTML files in logic JS file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,3 +3,11 @@ let currentQuestionIndex = 0;
 let time = questions.length * 15;
 let timerID;
 
+//Add Elements from HTML files:
+let questionsElement = document.getElementById("questions");
+let timerElement = document.getElementById("time");
+let choicesElement = document.getElementById("choices");
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialElement = document.getElementById("initials");
+let feedBackElement = document.getElementById("feedback");
