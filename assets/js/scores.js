863db056adab8a68ebbccc0d683402c93c9af7cc
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-01-20 19:04:03 +0000

    Added code to printHighScores function in the scores JS file

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,17 @@
 function printHighScores(){
-    
+    let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    })
+
+    highscores.forEach(function(score) {
+        let li = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+    })
 }
 
 function claerHighScore(){
